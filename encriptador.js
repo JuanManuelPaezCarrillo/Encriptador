ef3ae167ba4c34e86d4390bccdc9018583e61c5b
JuanManuelPaezCArrillo <paez3939@gmail.com>
2024-02-27 01:36:42 -0500

    ultima actualización

--- encriptador.js
+++ encriptador.js
@@ -10,33 +10,40 @@ let textoEncriptado = texto
     .replace(/o/gi,"ober")
     .replace(/u/gi,"ufat"); 
 
-if (texto.length !=0){
+if (texto.trim().length !==0){
     document.getElementById("texto").value = textoEncriptado;
     document.getElementById("tituloMensaje").textContent = "Texto encriptado con éxito";
     document.getElementById("parrafo").textContent = "¡AVISO! puedes continuar encriptando o da clic en desencriptar";
+    
 } else{
     tituloMensaje.textContent = "Ningún mensaje fue encontrado";
     parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";  
+    }    
 }
-}
+
 function desencriptar(){
     let texto = document.getElementById("texto").value;
     let tituloMensaje = document.getElementById("tituloMensaje");
     let parrafo = document.getElementById("parrafo");
 
-let textoEncriptado = texto
-    .replace(/ai/gi,"a")
-    .replace(/enter/gi,"e")
-    .replace(/imes/gi,"i")
+let textoDesencriptado = texto
+    .replace(/ufat/gi,"u")
     .replace(/ober/gi,"o")
-    .replace(/ufat/gi,"u");
+    .replace(/imes/gi,"i")
+    .replace(/enter/gi,"e")
+    .replace(/ai/gi,"a");
  
-    if (texto.length !=0){
-        document.getElementById("texto").value = textoEncriptado;
+    if (texto.trim().length !==0){
+        document.getElementById("texto").value = textoDesencriptado;
         document.getElementById("tituloMensaje").textContent = "Texto desencriptado con éxito";
         document.getElementById("parrafo").textContent = "¡Muy bien! Puedes ingresar un nuevo texto";
+        setTimeout(function() {
+            window.location.reload(); // Recargar la página después de 1000 milisegundos (1 segundo)
+        }, 10000);
     } else{
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";       
-    }
-}
+    }   
+}  
+
+    
